f1179dc2c2537024a28f4f3774e89c8acb0eabe9
Togrul Mardiyev <toghrulmardiyev@gmail.com>
2026-02-02 14:48:16 +0400

    reducing the amount of write calls

--- exit.c
+++ exit.c
@@ -1,6 +1,7 @@
 #include <termios.h>
 #include <errno.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -30,6 +31,14 @@ struct editorConfig {
 };
 
 struct editorConfig E;
+
+// 'dynamic' string struct
+struct abuf {
+    char *b;
+    int len;
+};
+// constructor for our abuf struct
+#define ABUF_INIT {NULL, 0} // pointer to null, length = 0
 
 void die(const char *s);
 void disableRawMode();
@@ -102,21 +111,19 @@ int getCursorPosition(int *rows, int *cols) {
         i++;
     }
 
-    // indicate the end of the buffer string using null terminator
-    buf[i] = '\0';
-    // we start printing from the 2nd character of the buffer
-    // this is becuase the 1st byte of the reply is the ESC character
-    // byte 27 in ASCII, which isn't printable so we skip it.
-    printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
-
-    editorReadKey();
+    buf[i] = '\0'; // indicate the end of the buffer string using null terminator
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1; // check that we got a correct reply
+    // Start reading the input string stored in the buffer from the 3rd character
+    // Then we store the row and column amounts into the specified variables
+    // This is done by matching the buffer string against the format string
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
     
-    return -1;
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
-    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) 
             return -1;
 
@@ -129,11 +136,45 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/***  Dynamic string functions  ***/
+
+// appends the given message from the given s string with the length len
+// into our dynamic string array ab
+void abAppend(struct abuf *ab, const char *s, int len) {
+    // reallocate memory fr the already existing ab pointer
+    // given the string length. We just the given length with ab's length
+    // This is a temporary variable
+    char *new = realloc(ab->b, ab->len + len);
+
+    // if realloc fails, return.
+    if (new == NULL) return;
+
+    // so new has the old characters from the previous append
+    // we don't touch them, what we do is we travel to our wanted
+    // pointer position, and copy the string s starting from that point
+    memcpy(new + ab->len, s, len);
+    ab->b = new; // we copied the string s to new, so we have to remap ab->b to new
+    ab->len += len; // increase the length of the string (array)
+
+    return;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b); // self-explanatory
+    return;
+}
+
 /*** Functions for terminal output ***/
-void editorDrawRows() { // draws '~' VIM style 
+void editorDrawRows(struct abuf *ab) { // draws '~' VIM style 
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        abAppend(ab, "~", 1);
+
+        // The last "\r\n" causes the terminal to scroll, pushing 
+        // the final ~ off the screen, so we skip printing it on the last row.
+        if(y < E.screenrows - 1){
+            abAppend(ab, "\r\n", 2);
+        }
     }
 }
 
@@ -143,11 +184,17 @@ void clearScrean(){
 }
 
 void editorRefreshScreen(){
-    clearScrean();
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab, "\x1b[2J", 4); // clear Terminal display
+    abAppend(&ab, "\x1b[H", 3); // put cursor to the beginning
     
-    editorDrawRows();
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3); // put cursor back after drawing '~'
     
-    write(STDOUT_FILENO, "\x1b[H", 3); // put cursor back after drawing '~'
+    write(STDOUT_FILENO, ab.b, ab.len); // one write call instead of multiple ones
+    abFree(&ab);
 }
 
 

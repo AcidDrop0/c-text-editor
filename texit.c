5df12a0059ff90e6fae28cb6e9c523a47b318553
Togrul Mardiyev <toghrulmardiyev@gmail.com>
2026-02-06 12:10:30 +0400

    added special keys' functionalit (arrow keys, PgUp and PgDn, Home and End keys)

--- texit.c
+++ texit.c
@@ -26,11 +26,19 @@
 #define CTRL_KEY(k) ((k) & 0x1f) // Simple macro for better understanding
 
 // row = y; column = x
-enum editorKey {
+enum editorKey { // important functional keys
     ARROW_LEFT = 1000, // choosing large enough number, so there are no conflicts
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+
+    PAGE_UP,
+    PAGE_DOWN,
+
+    HOME_KEY,
+    END_KEY,
+
+    DEL_KEY
 };
 
 // Convenient struct to store everything related to our terminal settings
@@ -119,11 +127,40 @@ int editorReadKey() {
         if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if(seq[0] == '['){
-            switch (seq[1]){
-                case 'A': return ARROW_UP; // ESC [ A --> Up arrow key
-                case 'B': return ARROW_DOWN; // ESC [ B --> Down arrow key
-                case 'C': return ARROW_RIGHT; // ESC [ C --> Right arrow key
-                case 'D': return ARROW_LEFT; // ESC [ D --> Left arrow key
+            if(seq[1] >= '0' && seq[1] <= '9'){ // If ESC [ num, where num is btwn 0 and 9 
+
+                // if read times out
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+
+                if(seq[2] == '~'){
+                    switch (seq[1]){
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;   // ESC [ 3 ~ 
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;   // ESC [ 5 ~
+                        case '6': return PAGE_DOWN; // ESC [ 6 ~
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+
+            }
+            else{
+                switch (seq[1]){
+                    case 'A': return ARROW_UP;    // ESC [ A --> Up arrow key
+                    case 'B': return ARROW_DOWN;  // ESC [ B --> Down arrow key
+                    case 'C': return ARROW_RIGHT; // ESC [ C --> Right arrow key
+                    case 'D': return ARROW_LEFT;  // ESC [ D --> Left arrow key
+
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        }
+        else if(seq[0] == 'O'){
+            switch (seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
@@ -301,7 +338,24 @@ void editorProcessKeypress(){
             clearScreen();
             exit(0);
             break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screencols-1;
+            break;
         
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {   // Equate page-up / page-down amount to rows of the terminal screen
+                int times = E.screenrows; 
+                while(times--){ // while times != 0  -->  times -= 1
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                }
+            }
+
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
